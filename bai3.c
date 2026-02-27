ad3cad05d5f5093c5ca98bf2373e5b7c01fbdcc3
DoanLong6677 <phanlacdoanlongdl1@gmail.com>
2026-02-27 16:18:50 +0700

    nộp bài Lab7

--- bai3.c
+++ bai3.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: Phan Lạc Đoàn Long
+ * MSSV:      PS47837
+ * Lớp:       CS21302
  *****************************************************************************/
 
 //  BÀI 3: XÂY DỰNG CHƯƠNG TRÌNH SẮP XẾP CHUỖI THEO CHỮ CÁI 
@@ -10,3 +10,38 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[5][20];
+    char temp[20];
+    int i, j;
+
+    // Nhập 5 chuỗi từ người dùng
+    for (i = 0; i < 5; i++) {
+        printf("Xin moi nhap chuoi thu %d: ", i + 1);
+        gets(s[i]);
+    }
+
+    // thuật toán sắp xếp bubble sort
+    for (i = 0; i < 4; i++) {
+        for (j = 1; j < 5; i++) {
+            // so sánh hai chuỗi bằng strcmp
+            if (strcmp(s[j - 1],s[j]) > 0) {
+                // hoán đổi chuỗi
+                strcpy(temp, s[j - 1]);
+                strcpy(s[j - 1], s[j]);
+                strcpy(s[j], temp);
+            }
+        }
+    }
+    
+    // In ra các chuỗi đã được sắp xếp
+    printf("\nCac chuoi sau khi sap xep:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%s\n", s[i]);
+    }
+    
+    return 0;
+}
